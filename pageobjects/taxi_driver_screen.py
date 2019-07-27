f0a8b42172ef5bb3f085470e80d6e6a97c343be2
Mahya Khazaei <m.khazaei@Tap30.net>
2019-07-27 12:14:47 +0430

    signin page correct senario done

--- pageobjects/taxi_driver_screen.py
+++ pageobjects/taxi_driver_screen.py
@@ -1,4 +1,4 @@
-from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.common.mobileby import MobileBy,By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
@@ -6,15 +6,57 @@ from selenium.webdriver.common.keys import Keys
 class TaxiDriverscreen:
     def __init__(self,driver):
         self.driver = driver
-        self.etext_signin_phonenumber = WebDriverWait(self.driver.instance, 5).until(
+        self.etext_signin_phone_number = WebDriverWait(self.driver.instance, 3).until(
             EC.visibility_of_element_located((
                 MobileBy.ID, "taxi.tap30.driver:id/edittext_signinphonenumber_number")))
-        self.sbutton_signin_phonenumber = WebDriverWait(self.driver.instance, 5).until(
+        self.sbutton_signin_phone_number = WebDriverWait(self.driver.instance, 3).until(
             EC.visibility_of_element_located((
                 MobileBy.ID, "taxi.tap30.driver:id/smartbutton_signinphonenumber")))
 
+    def verification_page_loaded(self):
+        self.sbutton_signin_verificationcode = WebDriverWait(self.driver.instance, 5).until(
+            EC.visibility_of_element_located((
+                MobileBy.ID, "taxi.tap30.driver:id/smartbutton_signinverificationcode")))
+        etexr_verification_xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText"
+        self.etext_verification_list = [
+            WebDriverWait(self.driver.instance, 5).until(
+                EC.visibility_of_element_located((
+                    MobileBy.XPATH, etexr_verification_xpath+"["+str(i)+"]"))) for i in range(1,6)
+        ]
+
     def signin_inter_phone_number(self,phone_number):
-        no = str(phone_number)
-        self.driver.instance.find_element(MobileBy.ID,"taxi.tap30.driver:id/edittext_signinphonenumber_number").set_value(phone_number)
-        print(self.etext_signin_phonenumber.text,phone_number)
-        assert "j" in self.etext_signin_phonenumber.text, "Clicked number wasn't reflected."
+        self.etext_signin_phone_number.clear()
+        self.etext_signin_phone_number.set_value(phone_number)
+        print("phone number",phone_number)
+        assert phone_number == self.etext_signin_phone_number.text, "insert phone number wasn't reflected."
+
+    def signin_clear_phone_number(self):
+        self.etext_signin_phone_number.clear()
+        assert "" == self.etext_signin_phone_number.text, "insert phone number wasn't reflected."
+
+    def signin_click_button(self):
+        self.sbutton_signin_phone_number.click()
+
+    def verification_inter_code(self,code):
+        #self.etext_signin_verificationcode.clear()
+        for i in range(5):
+            etext = self.etext_verification_list[i]
+            etext.set_value(code[i])
+        #self.etext_signin_verificationcode.set_value(code)
+        '''
+        etext_signin_verificationcode = ""
+        for i in range(5):
+            etext_signin_verificationcode += self.etext_verification_list[i].text
+
+        print("code",etext_signin_verificationcode)
+        assert code == etext_signin_verificationcode , "insert verification code wasn't reflected."
+        '''
+
+
+    def main_page_loaded(self):
+        self.button_online_offline = WebDriverWait(self.driver.instance, 3).until(
+            EC.visibility_of_element_located((
+                MobileBy.ID, "taxi.tap30.driver:id/toggle_bg")))
+
+
+
--- testcases/signin_test_cases.py
+++ testcases/signin_test_cases.py
@@ -3,8 +3,15 @@ import unittest
 from pageobjects.taxi_driver_screen import TaxiDriverscreen
 from webdriver.webdriver import Driver
 
+'''
+connection error
+wrong phone number
+wrong verification code
 
-class InitTestCases(unittest.TestCase):
+
+'''
+
+class SigninTestCases(unittest.TestCase):
 
     def setUp(self):
         self.driver = Driver()
@@ -15,10 +22,17 @@ class InitTestCases(unittest.TestCase):
         taxidriver = TaxiDriverscreen(self.driver)
         phone_number = '09998887766'
         taxidriver.signin_inter_phone_number(phone_number)
+        taxidriver.signin_click_button()
+
+        taxidriver.verification_page_loaded()
+
+        code = '15937'
+        taxidriver.verification_inter_code(code)
+        taxidriver.main_page_loaded()
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(InitTestCases)
+    suite = unittest.TestLoader().loadTestsFromTestCase(SigninTestCases)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 
